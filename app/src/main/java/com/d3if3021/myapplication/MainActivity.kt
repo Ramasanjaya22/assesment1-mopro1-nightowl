5b2b8c3498c4e738858f5ca29fef1245e6010bc3
Rama Sanjaya <ramasanjaya2202@gmail.com>
2023-03-27 03:08:46 +0700

    fitur pengaturan waktu tidur

--- app/src/main/java/com/d3if3021/myapplication/MainActivity.kt
+++ app/src/main/java/com/d3if3021/myapplication/MainActivity.kt
@@ -1,11 +1,61 @@
 package com.d3if3021.myapplication
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import com.d3if3021.myapplication.databinding.ActivityMainBinding
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSetAlarm.setOnClickListener {
+            // Mendapatkan waktu tidur yang dipilih oleh pengguna
+            val calendar = Calendar.getInstance()
+            calendar.set(Calendar.HOUR_OF_DAY, binding.timePicker.hour)
+            calendar.set(Calendar.MINUTE, binding.timePicker.minute)
+            calendar.set(Calendar.SECOND, 0)
+
+            // Membuat intent yang akan dipicu saat alarm berbunyi
+            val intent = Intent(this, AlarmReceiver::class.java)
+            val pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+            // Mendapatkan instance dari AlarmManager
+            val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+            // Menetapkan alarm pada waktu yang telah dipilih
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                alarmManager.setExactAndAllowWhileIdle(
+                    AlarmManager.RTC_WAKEUP,
+                    calendar.timeInMillis,
+                    pendingIntent
+                )
+            } else {
+                alarmManager.setExact(
+                    AlarmManager.RTC_WAKEUP,
+                    calendar.timeInMillis,
+                    pendingIntent
+                )
+            }
+
+            // Menampilkan pesan ke pengguna bahwa alarm telah diatur
+            val message = "Alarm telah diatur pada pukul ${binding.timePicker.hour}:${binding.timePicker.minute}"
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        }
     }
 }
+
